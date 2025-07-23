82c5049e86537a31310773673e631029dd7ca72e
MalikaChathushka <malika.cdabare@gmail.com>
2025-07-23 14:38:09 +0530

    Updated conftest.py file for generate HTNL reports

--- test_cases/conftest.py
+++ test_cases/conftest.py
@@ -1,6 +1,7 @@
 import pytest  # Importing pytest for test configuration and fixtures
 import undetected_chromedriver as uc  # Importing undetected_chromedriver to avoid bot detection by websites
 from selenium import webdriver  # Import selenium's webdriver for Firefox and Edge
+from pytest_metadata.plugin import metadata_key  # Importing metadata_key for pytest HTML reports
 
 # This function adds a custom command-line option to pytest for selecting the browser
 def pytest_addoption(parser):
@@ -30,3 +31,21 @@ def setup(browser):
 
     yield driver          # Provides the driver to the test function
     driver.quit()         # Ensures the browser is closed after
+
+
+
+######## for pytest html reports ########
+
+# This function is called to configure the metadata for the pytest HTML report.
+# It adds custom metadata entries like project name, module name, and tester's name.
+def pytest_configure(config):
+    config.stash[metadata_key] ['Project Name'] = 'nopCommerce Selenium Framework'  # Adds project name to metadata
+    config.stash[metadata_key] ['Module Name'] = 'Admin Login Tests'  # Adds module name to metadata
+    config.stash[metadata_key] ['Tester'] = 'Malika'  # Adds tester's name to metadata
+
+# This function is called to modify the metadata in the HTML report
+# It removes some default metadata entries that are not needed in the report
+@pytest.mark.optionalhook
+def pytest_metadata(metadata):
+    metadata.pop('JAVA_HOME', None)  # Removes JAVA_HOME from metadata
+    metadata.pop('Plugins', None)  # Removes Plugins from metadata
