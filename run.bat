4547d4ed06faacf14c24084b34b45ef38961681c
MalikaChathushka <malika.cdabare@gmail.com>
2025-07-25 21:55:23 +0530

    Updated bat file

--- run.bat
+++ run.bat
@@ -1,6 +1,5 @@
 @echo off
-set PATH=%PATH%;C:\Users\malik\AppData\Roaming\Python\Python311\Scripts
-@REM call venv\Scripts\activate
+call venv\Scripts\activate
 pytest -s -v -m "sanity" --html reports/sanity_test_report.html .\test_cases --browser chrome
 pytest -s -v -m "sanity and regression" --html reports/sanity_and_regression_report.html .\test_cases --browser chrome
 pause
